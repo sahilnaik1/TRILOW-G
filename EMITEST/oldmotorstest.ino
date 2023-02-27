3e4e9449a45cdc4bf1683b9fa1db4727458c468b
sahilnaik1 <59897263+sahilnaik1@users.noreply.github.com>
2023-02-26 20:01:59 -0800

    Add files via upload

---
+++ EMITEST/oldmotorstest.ino
@@ -0,0 +1,34 @@
+
+
+#include "oldmotors.h"
+
+void setup()
+{
+  SPI.begin();
+  
+  initializeRP();
+  initializeTED();
+
+  initializeTEDDCM();
+}
+
+void loop()
+{
+  openLoopDriveRP(DOWN,1 , 0.25, 64);
+  delay(500);
+  openLoopDriveRP(UP, 1, 2, 16);  
+  delay(500);
+
+
+
+  openLoopDriveTED(UP, 0.25, 0.25, 64);
+  delay(500);
+  TEDDCM(DOWN, 5); // for now just running for 5s
+  delay(500);
+  openLoopDriveTED(UP, 0.5, 0.25, 64);
+  delay(500);
+  TEDDCM(DOWN, 5); // for now just running for 5s
+  delay(500);
+  openLoopDriveTED(DOWN, 0.75,  1, 64);
+  delay(500);
+}
