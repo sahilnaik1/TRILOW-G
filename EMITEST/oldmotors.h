73689881fc14dd017ba6b42a54ae12586393911f
sahilnaik1 <59897263+sahilnaik1@users.noreply.github.com>
2023-02-27 16:09:37 -0800

    Update oldmotors.h

--- EMITEST/oldmotors.h
+++ EMITEST/oldmotors.h
@@ -4,10 +4,69 @@
 HighPowerStepperDriver RP, TED;
 #include <SparkFun_TB6612.h>
 
+#define AIN1 30
+#define BIN1 35
+#define AIN2 29
+#define BIN2 34
+#define PWMA 28
+#define PWMB 33
+#define STBY 32
+const int offsetA = 1;
+const int offsetB = 1;
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY); // ted
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY); // vibe
+
 
 const uint8_t ChipSelectRP = 41;
 const uint8_t ChipSelectTED = 37;
 
+float t1; //t1 operates timers on Solenoid Valve
+float t2; //t2 operates timers on Vibration Motors
+
+float timeOpenSV = 150.0; //ms Solenoid Valve
+float timeCloseSV = 100.0; //ms Solenoid Valve
+float timeOpenVM = 500.0; //ms Vibration Motor
+float timeCloseVM = 1000.0; //ms Vibration Motor
+float totalTime = 10.0; //s Fluidization length
+bool check = true;
+
+// fluidize
+void fluidize(unsigned long currtime){
+  float totalTime = currtime + totalTime;
+  if (float(millis())/1000.0 > totalTime){ // seconds
+ 
+    return; //exit program after ~totalTime seconds of fluidization
+  }
+  if (check){
+    float t1 = float(millis());
+    float t2 = float(millis());
+    check = false;
+  }
+  if (t2 < float(millis()) && float(millis()) < t2 + timeOpenVM){
+    motor2.drive(200);
+  }
+  else if (t2 + timeOpenVM < float(millis()) && float(millis()) < t2 + timeOpenVM + timeCloseVM){
+    motor2.brake();
+  }
+
+  
+  if (t1 < float(millis()) && float(millis()) < t1 + timeOpenSV){
+    digitalWrite(14, HIGH);
+  }
+  else if (t1 + timeOpenSV < float(millis()) && float(millis()) < t1 + timeOpenSV +timeCloseSV){
+    digitalWrite(14, LOW);
+  }
+
+  
+  if (float(millis()) > t1 + timeOpenSV+timeCloseSV) {
+    t1 = float(millis());
+  }
+  if (float(millis()) > t2 + timeOpenVM + timeCloseVM) {
+    t2 = float(millis());
+  }
+}
+// stepper
 enum Direction : uint8_t
 {
     DOWN, UP, CCW, CW
@@ -83,31 +142,18 @@ void initializeTED()
 //  END OF STEPPER CODE
 /////////////////////////////////////////////////////////////////////////
 
-const uint8_t AIN1 = 30;
-const uint8_t AIN2 = 29;
-const uint8_t PWMA = 28;
-const uint8_t STBY = 32;
-const uint8_t offsetA = 1;
-
-const uint8_t EncA = 9;
-const uint8_t EncB = 8;
+//const uint8_t AIN1 = 30;
+//const uint8_t AIN2 = 29;
+//const uint8_t PWMA = 28;
+//const uint8_t STBY = 32;
+//const uint8_t offsetA = 1;
+//
+//const uint8_t EncA = 9;
+//const uint8_t EncB = 8;
 
 //Encoder myEnc(EncA, EncB);
 
-void initializeTEDDCM()
-{
-//  pinMode(AIN1,OUTPUT);
-//  pinMode(AIN2,OUTPUT);
-//  pinMode(PWMA,OUTPUT);
-//  pinMode(STBY,OUTPUT);
-//
-//  digitalWrite(AIN1, LOW);
-//  digitalWrite(AIN2, LOW);
-//  digitalWrite(PWMA, HIGH);
-//  digitalWrite(STBY, HIGH);
-  Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 
-}
 
 long oldPosition  = -999;
 
@@ -115,48 +161,19 @@ enum OffState : uint8_t
 {
   STOP, STANDBY
 };
-//
-//float readEncDeg()
-//{
-//  return myEnc.read() * 0.288;
-//}
+
 
 void TEDDCM(Direction dir, double dist, short spd = 1.0, OffState os = STOP)
 {
-
-//  double deg = 114.591559 * dist * 4;
-//  int pulse = 22.13700572 * spd * 4;
-//
-//  float initPos = readEncDeg();
-//
-//    
-//  digitalWrite(STBY, HIGH);
-//  analogWrite(PWM, spd);
-//  
-//  if(dir == CCW)
-//  {
-//    digitalWrite(IN1, HIGH);
-//    digitalWrite(IN2, LOW);
-//
-//    while((readEncDeg() - initPos) > -deg) continue;
-//  }
-//  else
-//  {
-//    digitalWrite(IN1, LOW);
-//    digitalWrite(IN2, HIGH);
-//
-//    while((readEncDeg() - initPos) < deg) continue;
-//  }
-//  
-//  digitalWrite(IN1, LOW);
-//  digitalWrite(IN2, LOW);
-//
-//  analogWrite(PWM, 0);
-//
-//  if(os == STANDBY) digitalWrite(STBY, LOW);
-    Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
-
-    motor1.drive(155,5000);
-    motor1.brake();
+    if (dir == DOWN) {
+      motor1.drive(55,5000);
+      motor1.brake();
+      return;
+    }
+    else if (dir == UP){
+      motor1.drive(55,5000);
+      motor1.brake();
+      return;
+    }
 
 }
